14e514457bfb48e6470a074b6c239d68c5fc0386
Benjamin Laborderie <benjamin.laborderie@gmail.com>
2023-10-09 15:49:42 +0200

    Add filter button js and first btn css

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -3,76 +3,102 @@
 /////////////////////////////////
 * >> PROJECT SECTION
 *****************/
-const GALLERY_API_URL = "http://localhost:5678/api/works";
 
-const gallery = document.querySelector(".gallery");
-const portFolio = document.getElementById("portfolio");
-console.log(portFolio)
-let data = null;
+////////////////////////////////////////////////////////////////////////////////////////////
+// General
 
-const allFilters = [
-    {
-        filterName : "Tous"
-    },
-    {
-        filterName : "Objets"
-    },
-    {
-        filterName : "Appartements"
-    },
-    {
-        filterName : "Hôtels & restaurants"
-    }
-];
+const GALLERY_API_URL = "http://localhost:5678/api/works",
+    CATEGORY_API_URL = "http://localhost:5678/api/categories";
 
-async function fetchProjectData() { //Async function for call dynamic project from API
-    try {
-        const callProject = await fetch(GALLERY_API_URL);
-        console.log(callProject);
+const portFolio = document.getElementById("portfolio"),
+     gallery = document.querySelector(".gallery");
 
-        data = await callProject.json(); // wait promise to analysis json response
-        console.log(data)
-    } catch (error) { //Append error text if fetch does not work
-        console.log({error})
-        //Create error on screen >
-        const errorProject = document.createElement("p");
-        errorProject.classList.add("error");
-        errorProject.innerText = "Something went wrong :( Tentative de reconnexion..."; //TODO : Créer une tentative de reconnexion******************************************************
-        //Prevents error stacking >
-        const existingError = document.querySelector(".error");
-        if (existingError) {
-            existingError.remove();
-        }
-        //Add error to DOM >
-        gallery.appendChild(errorProject);
-    }
-}
+const mainDataAPI = await fetch (GALLERY_API_URL); //Call all works from API
 
-await fetchProjectData() //await for no error on the function projectGeneration > data load
-projectGeneration()
+let mainDataJSON = await mainDataAPI.json(); //waiting promise from GALLERY_API_URL
+console.log(mainDataJSON);
 
-function projectGeneration() { //Function for generate dynamic gallery elements
-    console.log({data})
-    for (let i = 0; i < data.length; i++) {
+const categoryDataAPI = await fetch (CATEGORY_API_URL); // Call only category from API
+
+let categoryDataJSON = await categoryDataAPI.json(); //waiting promise from CATEGORY_API_URL
+console.log(categoryDataJSON);
+
+// Initial Image Gallery function generation
+function galleryCreation() {
+    for (let i = 0; i < mainDataJSON.length; i++) {
         const figureProject = document.createElement("figure");
         const galleryImg = document.createElement("img");
-        const galleryTxt = document.createElement("figcaption")
-        galleryImg.src = data[i].imageUrl;
-        galleryTxt.innerText = data[i].title;
-        gallery.appendChild(figureProject)
-        figureProject.appendChild(galleryImg)
-        figureProject.appendChild(galleryTxt)
+        const galleryTxt = document.createElement("figcaption");
+        galleryImg.src = mainDataJSON[i].imageUrl;
+        galleryTxt.innerText = mainDataJSON[i].title;
+        figureProject.appendChild(galleryImg);
+        figureProject.appendChild(galleryTxt);
         gallery.appendChild(figureProject);
     }
 }
 
-function generateFilters() { //function for generate dynamic filters menu
-    const filterContainer = document.createElement("div")
+galleryCreation()
 
-    for (let i = 0; i < allFilters.length; i++) {
-        const filters = document.createElement("button")
-        filterContainer.appendChild(filters)
+//Category filters function generation
+function filtersCreation() {
+    //Creation of filter container
+    const filterContainer = document.createElement("div");
+    portFolio.insertBefore(filterContainer, gallery);
+    filterContainer.setAttribute("id", "filter-btn-container");
+    filterContainer.className = "filter-container";
+
+    //Creation of button "all"
+    const btnAll = document.createElement("button");
+    filterContainer.appendChild(btnAll);
+    btnAll.innerText = "Tous";
+    btnAll.setAttribute("id", "filter-btn0");
+    btnAll.className = "filter-btn active-btn";
+
+    //Creation of category filters
+    for (let i = 0; i < categoryDataJSON.length; i++) {
+        const filters = document.createElement("button");
+        filterContainer.appendChild(filters);
+        filters.innerText = categoryDataJSON[i]["name"];
+        filters.setAttribute("id", "filter-btn"+categoryDataJSON[i]["id"]);
+        filters.className = "filter-btn";
+        filters.setAttribute("data-category", categoryDataJSON[i]["id"]);
     }
 }
 
-generateFilters()
+filtersCreation()
+
+//Btn clicked and update Gallery
+
+const btnClicked = document.querySelectorAll(".filter-btn");
+
+btnClicked.forEach(el => el.addEventListener("click", event => {
+    const categoryId = event.target.getAttribute("data-category");
+    const works = mainDataJSON.filter(work => categoryId == work.categoryId );
+
+    btnClicked.forEach(btn => btn.classList.remove("active-btn"));
+
+    // Ajoute la classe active-btn uniquement au bouton actuellement cliqué
+    event.target.classList.add("active-btn");
+
+    console.log(works);
+    if (works.length > 0) {
+        console.log(works);
+        galleryUpdate(works);
+    } else {
+        gallery.innerHTML = "";
+        galleryCreation();
+}}))
+
+function galleryUpdate(works) {
+    gallery.innerHTML = ""
+    for (let i = 0; i < works.length; i++) {
+        const figureProject = document.createElement("figure");
+        const galleryImg = document.createElement("img");
+        const galleryTxt = document.createElement("figcaption");
+        galleryImg.src = works[i].imageUrl;
+        galleryTxt.innerText = works[i].title;
+        figureProject.appendChild(galleryImg);
+        figureProject.appendChild(galleryTxt);
+        gallery.appendChild(figureProject);
+    }
+}
