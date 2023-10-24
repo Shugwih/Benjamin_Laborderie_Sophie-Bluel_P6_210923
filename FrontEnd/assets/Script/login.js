17b1248588887a21d43e3f751aebc34f4bd9d71d
Benjamin Laborderie <benjamin.laborderie@gmail.com>
2023-10-24 18:21:33 +0200

    Add modal with open/close

--- FrontEnd/assets/Script/login.js
+++ FrontEnd/assets/Script/login.js
@@ -3,41 +3,22 @@ const Inputemail = document.getElementById("login-email"),
 
 const submit = document.getElementById("connexion");
 
+const errorContainer = document.getElementById("error-container")
+
 const TOKEN_API_URL = "http://localhost:5678/api/users/login";
 console.log(TOKEN_API_URL)
-
 //Submit listener
 submit.addEventListener("click", (event) => {
     event.preventDefault();
-    logIn();
+    login();
 })
 
-//Connexion function
-async function logIn() {
+async function login() {
+    const email = Inputemail.value,
+        password = Inputpassword.value;
 
-    //Verify Email
-    /*  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (email.value.match(emailRegex)) {
-        console.log("Correct Email")
-        email.classList.remove("input-error")
-    } else {
-        console.log("Incorrect Email");
-        email.className = "input-error";
-    }
-    //Verify Password
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d!@#$%^&*()_+|~-]{5,}$/;
-    if (password.value.match(passwordRegex)) {
-        console.log("Correct Password")
-        password.classList.remove("input-error")
-    } else {
-        console.log("Incorrect Password")
-        password.className = "input-error";
-    }*/
-    const email = Inputemail.value;
-    const password = Inputpassword.value
-    const data = {email : email, password: password}
-    console.log(data)
-
+    const data = {email : email, password: password};
+    console.log(data);
     const config = {
         method: "POST",
         headers: {
@@ -45,19 +26,33 @@ async function logIn() {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(data)
-    }
-    const response = await fetch(TOKEN_API_URL, config);
-    const result = await response.json();
+    };
 
-    if (result.token) {
-        // stock token in localStorage
-        localStorage.setItem("authToken", result.token);
-        console.log("Token stocké.");
-
-        // user redirection to home
-        window.location.href = "./index.html"; // Remplacez 'page-d-accueil.html' par l'URL de votre page d'accueil
-    } else {
-        console.error("Erreur lors de la connexion.");
+    try {
+        const response = await fetch(TOKEN_API_URL, config);
+        if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}`);
+        }
+        const result = await response.json(); // Récupérer les données de réponse
+        if (result.token) {
+            // stock token in localStorage
+            localStorage.setItem("authToken", result.token);
+            console.log("Token stored.");
+    
+            // user redirection to home
+            window.location.href = "./index.html";
+        }
+    } catch (error) {
+        errorContainer.innerHTML = ""
+        const Usererror = document.createElement('p');
+            Usererror.innerText = 'Email ou mot de passe incorrect';
+            Usererror.style.textAlign = 'center';
+            Usererror.style.color = 'red';
+            console.log(Usererror)
+            console.log(errorContainer)
+            errorContainer.appendChild(Usererror);
+            console.log(errorContainer)
+            console.error("Erreur lors de la connexion.");
     }
 }
 
@@ -77,3 +72,81 @@ function isAlreadyConnect() {
 }
 
 isAlreadyConnect();
+
+//Connexion function
+/*
+async function logIn() {
+
+    const email = Inputemail.value;
+    const password = Inputpassword.value
+    const data = {email : email, password: password}
+    console.log(data)
+
+    const config = {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }
+    const response = await fetch(TOKEN_API_URL, config);
+    const result = await response.json();
+
+    if (result.token) {
+        // stock token in localStorage
+        localStorage.setItem("authToken", result.token);
+        console.log("Token stored.");
+
+        // user redirection to home
+        window.location.href = "./index.html"; //
+    } else {
+        console.error("Erreur lors de la connexion.");
+    } 
+    //Verify Email
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (Inputemail.value.match(emailRegex)) {
+        console.log("Correct Email")
+        Inputemail.classList.remove("input-error")
+    } else {
+        console.log("Incorrect Email");
+        Inputemail.className = "input-error";
+
+        Inputemail.addEventListener("keypress", () => {
+            Inputemail.classList.remove("input-error");
+        });
+    }
+    //Verify Password
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d!@#$%^&*()_+|~-]{5,}$/;
+    if (Inputpassword.value.match(passwordRegex)) {
+        console.log("Correct Password")
+        Inputpassword.classList.remove("input-error")
+        
+    } else {
+        console.log("Incorrect Password")
+        Inputpassword.className = "input-error";
+        
+
+        Inputpassword.addEventListener("keypress", () => {
+            Inputpassword.classList.remove("input-error");
+        });
+    }
+}
+
+//Client already logged function
+function isAlreadyConnect() {
+    // recover token from localstorage
+    const authToken = localStorage.getItem("authToken");
+
+    if (authToken) {
+        // User connected
+        console.log("L'utilisateur est connecté.");
+        // avaible to use authToken
+    } else {
+        // User disconnected
+        console.log("L'utilisateur n'est pas connecté.");
+    }
+}
+
+isAlreadyConnect();*/
+
--- FrontEnd/assets/Script/script.js
+++ FrontEnd/assets/Script/script.js
@@ -6,111 +6,6 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // General
-/*
-const GALLERY_API_URL = "http://localhost:5678/api/works",
-    CATEGORY_API_URL = "http://localhost:5678/api/categories";
-
-const portFolio = document.getElementById("portfolio"),
-     gallery = document.querySelector(".gallery");
-
-const mainDataAPI = await fetch (GALLERY_API_URL); //Call all works from API
-
-let mainDataJSON = await mainDataAPI.json(); //waiting promise from GALLERY_API_URL
-console.log(mainDataJSON);
-
-const categoryDataAPI = await fetch (CATEGORY_API_URL); // Call only category from API
-
-let categoryDataJSON = await categoryDataAPI.json(); //waiting promise from CATEGORY_API_URL
-console.log(categoryDataJSON);
-
-// Initial Image Gallery function generation
-function galleryCreation() {
-    gallery.innerHTML = ""
-    for (let i = 0; i < mainDataJSON.length; i++) {
-        const figureProject = document.createElement("figure");
-        const galleryImg = document.createElement("img");
-        const galleryTxt = document.createElement("figcaption");
-        galleryImg.src = mainDataJSON[i].imageUrl;
-        galleryTxt.innerText = mainDataJSON[i].title;
-        figureProject.appendChild(galleryImg);
-        figureProject.appendChild(galleryTxt);
-        gallery.appendChild(figureProject);
-    }
-}
-
-galleryCreation()
-
-//Category filters function generation
-function filtersCreation() {
-    //Creation of filter container
-    const filterContainer = document.createElement("div");
-    portFolio.insertBefore(filterContainer, gallery);
-    filterContainer.setAttribute("id", "filter-btn-container");
-    filterContainer.className = "filter-container";
-
-    //Creation of button "all"
-    const btnAll = document.createElement("button");
-    filterContainer.appendChild(btnAll);
-    btnAll.innerText = "Tous";
-    btnAll.setAttribute("id", "filter-btn0");
-    btnAll.className = "filter-btn active-btn";
-
-    //Creation of category filters
-    for (let i = 0; i < categoryDataJSON.length; i++) {
-        const filters = document.createElement("button");
-        filterContainer.appendChild(filters);
-        filters.innerText = categoryDataJSON[i]["name"];
-        filters.setAttribute("id", "filter-btn"+categoryDataJSON[i]["id"]);
-        filters.className = "filter-btn";
-        filters.setAttribute("data-category", categoryDataJSON[i]["id"]);
-    }
-}
-
-filtersCreation()
-
-//Btn clicked and update Gallery
-
-const btnClicked = document.querySelectorAll(".filter-btn");
-
-btnClicked.forEach(el => el.addEventListener("click", event => {
-    const categoryId = event.target.getAttribute("data-category");
-    const works = mainDataJSON.filter(work => categoryId == work.categoryId );
-
-    btnClicked.forEach(btn => btn.classList.remove("active-btn"));
-
-
-    event.target.classList.add("active-btn");
-
-    console.log(works);
-    if (works.length > 0) {
-        console.log(works);
-        galleryUpdate(works);
-    } else {
-        galleryCreation();
-}}))
-
-function galleryUpdate(works) {
-    gallery.innerHTML = ""
-    for (let i = 0; i < works.length; i++) {
-        const figureProject = document.createElement("figure");
-        const galleryImg = document.createElement("img");
-        const galleryTxt = document.createElement("figcaption");
-        galleryImg.src = works[i].imageUrl;
-        galleryTxt.innerText = works[i].title;
-        figureProject.appendChild(galleryImg);
-        figureProject.appendChild(galleryTxt);
-        gallery.appendChild(figureProject);
-    }
-}
-*/
-/***************** 
-* > HOME PAGE SCRIPT / API DATA EXTRACT / SCRIPT IMPORT
-/////////////////////////////////
-* >> PROJECT SECTION
-*****************/
-
-////////////////////////////////////////////////////////////////////////////////////////////
-// General
 
 const GALLERY_API_URL = "http://localhost:5678/api/works",
     CATEGORY_API_URL = "http://localhost:5678/api/categories";
@@ -200,3 +95,68 @@ btnClicked.forEach(el => el.addEventListener("click", event => {
 // Gallery init
 galleryCreation();
 
+function headerNavAction() {
+    const userToken = localStorage.getItem("")
+    const headerNav = document.querySelector("header"),
+        headerLink = headerNav.querySelectorAll("li");
+    for (let i=0; i < headerLink.length; i++) {
+        const headerLinkId = "navActionBtn" + [i];
+
+        headerLink[i].setAttribute("id", headerLinkId)
+    }
+    const projectActionBtn = document.getElementById("navActionBtn0").onclick = () => {
+        location.href ="#portfolio"
+    }
+    const contactActionBtn = document.getElementById("navActionBtn1").onclick = () => {
+        location.href ="#contact"
+    }
+    const signInActionBtn = document.getElementById("navActionBtn2").onclick = () => {
+        location.href = "./login.html"
+    }
+    const instagramActionBtn = document.getElementById("navActionBtn3").onclick = () => {
+        location.href = ("https://www.instagram.com/")
+    }
+}
+headerNavAction()
+
+//MODAL
+
+let modal = null
+
+const openmodal = function(e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute("href"));
+    target.style.display = null;
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", true);
+    modal = target;
+    modal.addEventListener("click", closeModal);
+    modal.querySelector(".js-admin-modal-close").addEventListener("click", closeModal)
+    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+}
+
+const closeModal = function(e) {
+    if (modal === null) return;
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", true);
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click", closeModal);
+    modal.querySelector(".js-admin-modal-close").removeEventListener("click", closeModal);
+    modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
+    modal = null;
+}
+
+const stopPropagation = function(e) {
+    e.stopPropagation()
+}
+
+document.querySelectorAll(".js-modal-admin").forEach(a => {
+    a.addEventListener("click", openmodal)
+})
+
+window.addEventListener("keydown", function(e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e)
+    }
+})
